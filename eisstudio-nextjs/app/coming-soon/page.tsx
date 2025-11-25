5b15ffe31b4ab25eabb3c2d4460433ed639df5f6
chrisznb <chris@Christians-MacBook-Air.local>
2025-11-25 13:16:53 +0100

    Coming Soon: Eis-Icon entfernt

--- eisstudio-nextjs/app/coming-soon/page.tsx
+++ eisstudio-nextjs/app/coming-soon/page.tsx
@@ -39,19 +39,6 @@ export default function ComingSoonPage() {
             </p>
           </div>
 
-          {/* Ice cream icon */}
-          <div className="coming-soon-icon">
-            <svg viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M32 4C24.268 4 18 10.268 18 18v2h28v-2c0-7.732-6.268-14-14-14z" fill="#F2C23D"/>
-              <path d="M18 20h28v8c0 2.21-1.79 4-4 4H22c-2.21 0-4-1.79-4-4v-8z" fill="#F7F4E9"/>
-              <path d="M24 32l8 28 8-28H24z" fill="#D4A574"/>
-            </svg>
-          </div>
-
-          {/* Social hint */}
-          <p className="coming-soon-hint">
-            Folge uns für Updates
-          </p>
         </div>
 
         {/* Footer with legal links */}
